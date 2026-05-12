f25be11f068768d4952002f75a261747b911f030
Nikhil-28-vinayak <vinayaknikhil2224@gmail.com>
2026-05-12 17:00:06 +0530

    completed history, management and payement screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fieldguard/presentation/screens/alert_notification_screen/notification_screen.dart';
 import 'package:fieldguard/presentation/screens/dashboard_sscreen/admin_dashboard/dashboard_screen.dart';
 import 'package:fieldguard/presentation/notifiers/signup_notifier.dart';
 import 'package:fieldguard/presentation/screens/dashboard_sscreen/manager_dashboard/mdashboard_screen.dart';
@@ -5,7 +6,9 @@ import 'package:fieldguard/presentation/screens/fraud_alert_screen/fraud_alert_s
 import 'package:fieldguard/presentation/screens/live_team_map_screen/live_team_map_screen.dart';
 import 'package:fieldguard/presentation/screens/payment_overview_screen/payment_overview_screen.dart';
 import 'package:fieldguard/presentation/screens/representative_detail_screen.dart/representative_detail_screen.dart';
+import 'package:fieldguard/presentation/screens/shop_management_screen/shop_management_screen.dart';
 import 'package:fieldguard/presentation/screens/signup_screen/signup_screen.dart';
+import 'package:fieldguard/presentation/screens/visit_history_screen/visit_history_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:provider/provider.dart';
@@ -13,9 +16,7 @@ import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 
 void main() {
-  runApp(
-    const ProviderScope(child: MyApp()),
-  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends ConsumerWidget {
@@ -26,13 +27,17 @@ class MyApp extends ConsumerWidget {
     // The router is provided by Riverpod so it can react to auth state changes.
     final router = ref.watch(goRouterProvider);
 
-    return MaterialApp.router(
-      title: 'FieldGuard',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.light,
-      routerConfig: router,
-    );
+    // return MaterialApp.router(
+    //   title: 'FieldGuard',
+    //   debugShowCheckedModeBanner: false,
+    //   theme: AppTheme.light,
+    //   routerConfig: router,
+    // );
 
-    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true, fontFamily: 'Roboto'),
+      home: VisitHistoryScreen(),
+    );
   }
 }
